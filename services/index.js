df09916d39de6c673397f98db7f6e9ce01526327
Gin <eriegin96@gmail.com>
2022-03-03 21:18:23 +0700

    temp update

--- services/index.js
+++ services/index.js
@@ -1,6 +1,6 @@
 import { request, gql } from 'graphql-request';
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const graphqlAPI = process.env.GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
 	const query = gql`
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bg1: '#0F1624',
+				bg2: '#2c304d',
+				primary: '#f2f5f7',
+				accent1: '#fec576',
+				button: '#006bb8',
+			},
+		},
+		fontFamily: {
+			title: 'Space Grotesk, sans-serif',
+			main: 'Space Grotesk, sans-serif',
+		},
 	},
 	plugins: [],
 };
